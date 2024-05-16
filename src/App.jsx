edc0a4506a89c2960d87e4e25396ddb645338f12
gooncalvesgaby <gabrielle.goncalves@outlook.com.br>
2024-05-15 22:38:58 -0300

    inicio do projeto

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Header from "./Components/Header.jsx"
+import {createGlobalStyle} from "styled-components"
+
+const GlobalStyle = createGlobalStyle`
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+`
+
+function App() {
+    return(
+      <>
+      <Header/>
+      </>
+    )
+}
+
+export default App
---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as S from "./Style.jsx"
+
+function Header() {
+    return(
+        <S.Header>
+        <h1>ola eu sou o header</h1>
+        </S.Header>
+    )
+}
+
+export default Header
