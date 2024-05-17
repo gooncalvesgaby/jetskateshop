82d372592dfb762937b246c7f1a83d09d23f9551
gooncalvesgaby <gabrielle.goncalves@outlook.com.br>
2024-05-16 22:31:45 -0300

    ...

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React from "react"
 import * as S from "./Style.jsx"
+import Skate from "../assets/skate.png"
 
 function Header() {
     return(
         <S.Header>
-        <h1>ola eu sou o header</h1>
+            <img src={Skate} alt="imagem de um skate" />
         </S.Header>
     )
 }
--- src/Components/Style.jsx
+++ src/Components/Style.jsx
@@ -1,6 +1,5 @@
-import styled from "styled-components";
+import styled from "styled-components"
 
-export const Header = styled.h1`
-    color:pink;
-    
+export const Header = styled.img`
+ heigth: 5rem;
 `
