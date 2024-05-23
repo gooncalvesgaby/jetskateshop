910791b81feedcd25a055c54619d175f098ad554
gooncalvesgaby <gabrielle.goncalves@outlook.com.br>
2024-05-23 01:30:47 -0300

    inicio main

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -3,9 +3,11 @@ import * as S from "./MainStyle.jsx"
 
 function Main() {
     return(
-        <S.MainStyle>
-            <S.H2>oi sou main</S.H2>
-        </S.MainStyle>
+        <S.StyleMain>
+            <S.Div>
+                <S.H2>LIQUIDAÇÃO</S.H2>
+            </S.Div>
+        </S.StyleMain>
 
     )
 }
--- src/Components/MainStyle.jsx
+++ src/Components/MainStyle.jsx
@@ -1,5 +1,31 @@
-import styled from "styled-components"
+import styled, { keyframes } from "styled-components"
+
+export const StyleMain = styled.main`
+display: flex;
+`
+
+export const Div = styled.div`
+min-width: 100%;
+background-color: black;
+height: 5rem;
+display: flex;
+justify-content: start;
+align-items: center;
+`
 
 export const H2 = styled.h2`
-color: black;
+letter-spacing: 2rem;
+font-family: sans-serif;
+animation-name: cores, H2;
+animation-duration: 1s, 3s;
+animation-delay: 1s, 2s;
+animation-iteration-count: infinite, infinite;
+
+@keyframes cores{
+    from{color: #6d6b6beb}
+    to{color: #fff}
+}
 `
+
+
+
