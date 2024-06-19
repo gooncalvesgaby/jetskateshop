b42e984bf30cca1a4196e63543968475c4e7db23
gooncalvesgaby <gabrielle.goncalves@outlook.com.br>
2024-06-18 22:28:40 -0300

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./Components/Header.jsx"
 import Main from "./Components/Main.jsx"
+import Footer from "./Components/Footer.jsx"
 import {createGlobalStyle} from "styled-components"
 
 const GlobalStyle = createGlobalStyle`
@@ -17,6 +18,7 @@ function App() {
       <GlobalStyle/>
       <Header/>
       <Main/>
+      <Footer/>
       </>
     )
 }
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import * as S from "./FooterStyle.jsx"
+
+function Footer() {
+    return(
+        <S.StyleFooter>
+
+        </S.StyleFooter>
+
+    )
+}
+
+export default Footer
